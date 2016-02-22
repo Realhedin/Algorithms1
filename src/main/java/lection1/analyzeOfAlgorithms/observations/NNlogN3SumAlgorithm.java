c0c791506d5abb9274167ff77ac7b41c3269d358
dkorolev <korwork87@yahoo.com>
2016-02-22 21:35:43 +0300

    added better algorithm for 3sum problem.

--- src/main/java/lection1/analyzeOfAlgorithms/observations/NNlogN3SumAlgorithm.java
+++ src/main/java/lection1/analyzeOfAlgorithms/observations/NNlogN3SumAlgorithm.java
@@ -1,30 +1,31 @@
 package lection1.analyzeOfAlgorithms.observations;
 
+import lection1.analyzeOfAlgorithms.binarySearch.BinarySearchImpl;
+
 /**
  * 3sum question solution
- * with brute force algorithm.
+ * with advanced algorithm.
  *
- * Created by dkorolev on 2/21/2016.
+ * Created by dkorolev on 2/22/2016.
  */
-public class ThreeSumBrutoForce {
+public class NNlogN3SumAlgorithm {
 
     /**
      * number of three sum
      * that get 0.
-     * N^3 complexity (3 for loop)
+     * N^2*logN complexity (2 loops + binary search)
      *
      * @param a - input int[]
      * @return number.
      */
-    public static int count(int[] a) {
+    public static int countClever(int[] a) {
         int n = a.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        count++;
-                    }
+                int k = BinarySearchImpl.binarySearch(a, -(a[i] + a[j]));
+                if (k > j) {
+                    count++;
                 }
             }
         }
--- src/main/java/lection1/analyzeOfAlgorithms/observations/TestClient.java
+++ src/main/java/lection1/analyzeOfAlgorithms/observations/TestClient.java
@@ -1,16 +1,33 @@
 package lection1.analyzeOfAlgorithms.observations;
 
 /**
+ * Test client for 3Sum.
+ *
  * Created by dkorolev on 2/21/2016.
  */
 public class TestClient {
 
+    public static final int N = 2000;
+
     public static void main(String[] args) {
-        int[] input = {
-                30, -40, -20, -10, 40, 0, 10, 5};
+        //test
+//        int[] input = {
+//                30, -40, -20, -10, 40, 0, 10, 5};
+        int[] input = new int[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = i;
+        }
+
+        //N^3 algorithm
         long startTime = System.nanoTime();
         int result = ThreeSumBrutoForce.count(input);
         long totalTime = System.nanoTime() - startTime;
+        System.out.println("Number:"+result+" : "+totalTime);
+
+        //N^2*logN algorithm
+        startTime = System.nanoTime();
+        result = NNlogN3SumAlgorithm.countClever(input);
+        totalTime = System.nanoTime() - startTime;
         System.out.println("Number:"+result+" : "+totalTime);
 
     }
