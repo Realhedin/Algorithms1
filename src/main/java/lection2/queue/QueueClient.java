cfd5be63b4ddd6411379384ff79173d40d191d86
dkorolev <korwork87@yahoo.com>
2016-03-13 12:54:37 +0300

    Resizing array queue implementation

--- src/main/java/lection2/queue/QueueClient.java
+++ src/main/java/lection2/queue/QueueClient.java
@@ -1,6 +1,6 @@
 package lection2.queue;
 
-import lection2.queue.impl.LinkedListQueueImpl;
+import lection2.queue.impl.ResizingArrayQueueImpl;
 import lection2.queue.interfaces.QueueOfGenericItems;
 
 /**
@@ -14,7 +14,9 @@ public class QueueClient {
 
     public static void main(String[] args) {
 
-        QueueOfGenericItems<String> queue = new LinkedListQueueImpl<>();
+//        QueueOfGenericItems<String> queue = new LinkedListQueueImpl<>();
+
+        QueueOfGenericItems<String> queue = new ResizingArrayQueueImpl<>();
 
         for (String arg : args) {
             if (arg.equals("-")) {
---
+++ src/main/java/lection2/queue/impl/ResizingArrayQueueImpl.java
@@ -0,0 +1,72 @@
+package lection2.queue.impl;
+
+import lection2.queue.interfaces.QueueOfGenericItems;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by dkorolev on 3/13/2016.
+ */
+public class ResizingArrayQueueImpl<Item> implements QueueOfGenericItems<Item> {
+
+    private Item[] a;   //array
+    private int n;      //size
+    private int first;  //pointer to first element
+    private int last;   //pointer to last element
+
+
+    public ResizingArrayQueueImpl() {
+        this.a = (Item[]) new Object[2];
+        n = 0;
+        first = 0;
+        last = 0;
+    }
+
+    /**
+     * change size of underlying array.
+     * @param capacity - new size of array.
+     */
+    public void resize(int capacity) {
+        Item[] newCopy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            newCopy[i] = a[(first + i) % a.length];
+        }
+        a = newCopy;
+        first = 0;
+        last = n;
+    }
+
+    @Override
+    public void enqueue(Item item) {
+        if (n == a.length) {
+            resize(a.length * 2);
+        }
+        a[last++] = item;
+        n++;
+    }
+
+    @Override
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty.");
+        }
+        Item item = a[first];
+        a[first] = null;
+        n--;
+        first++;
+        if (n > 0 && n == a.length / 4) {
+            resize(a.length/2);
+        }
+        return item;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    @Override
+    public int size() {
+        return n;
+    }
+}
